d9aed77648287920334e2e6b10494a300cc72737
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-09-07 10:44:44 -0400

    Update gui.py
    about

--- gui.py
+++ gui.py
@@ -12,6 +12,11 @@ def helloCallback():
    tkMB.showinfo( "gui-app", "Hello, World!")
 def aboutCallback():
    tkMB.showinfo( "gui-app version", "gui-app 2.0, using TkInter")
+   tkMB.showinfo( "i am not affiliated with this website!", "thanks to tiny.cc/tkinterguide for providing a guide. other sites:")
+   tkMB.showinfo( "not affiliated with this one either", "tiny.cc/tkintertitle (see the one by user8875910) for the title bar")
+   tkMB.showinfo( "again, not affiliated", "https://shields.io/ for the README.md badges")
+   tkMB.showinfo("not affiliated", "the PSF and Tkinter — without you this wouldn't exist!")
+   tkMB.showinfo("thanks to all of you!", "Thanks so much!")
 def badaCallback():
    B3.pack()
 def notCodedCallback():
