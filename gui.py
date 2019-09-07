ac9bdb554e4d204405828de45f812831969cc46f
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-09-07 17:04:40 -0400

    Update gui.py—took me awhile.
    24 minutes later

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ import Tkinter
 import tkMessageBox
 t = Tkinter
 tkMB = tkMessageBox
+from Tkinter import Label
 
 
 
@@ -9,13 +10,14 @@ top = Tkinter.Tk()
 top.title('gui-app')
 
 def helloCallback():
-   #Text.pack
    tkMB.showinfo( "gui-app", "Hello, World!")
+   txt1.pack()
 def aboutCallback():
    tkMB.showinfo( "gui-app version", "gui-app 2.0, using TkInter")
    tkMB.showinfo( "i am not affiliated with this website!", "thanks to tiny.cc/tkinterguide for providing a guide. other sites:")
    tkMB.showinfo( "not affiliated with this one either", "tiny.cc/tkintertitle (see the one by user8875910) for the title bar")
    tkMB.showinfo( "again, not affiliated", "https://shields.io/ for the README.md badges")
+   tkMB.showinfo("dude do you still thing im affiliated with this company?", "https://pythonprogramming.net/tkinter-adding-text-images/")
    tkMB.showinfo("not affiliated", "the PSF and Tkinter -- without you this wouldn't exist!")
    tkMB.showinfo("thanks to all of you!", "Thanks so much!")
 def badaCallback():
@@ -32,9 +34,10 @@ BAbt = t.Button(top, text ="About...", command=aboutCallback)
 BSm = t.Button(top, text ="Show more...", command= badaCallback)
 BSem = t.Button(top, text ="Show even more...", command= showMoreCallback)
 BBada = t.Button(top, text ="Badaboom!", command=badaCB)
-#THello = t.Text ( top, "'Hello' button was pressed")
+txt1 = Label(top, text="'Hello' button was pressed")
+txt2 = Label(top, text="Welcome to gui-app!")
 
-
+txt2.pack()
 B.pack()
 BSm.pack()
 tkMB.showerror("Willkommen!", "Welcome to gui-app")
