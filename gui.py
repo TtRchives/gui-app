b61041e4631c7fed412415f6590a00aab1dfa3ee
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-09-07 09:56:26 -0400

    Update gui.py
    A lot of changes!

--- gui.py
+++ gui.py
@@ -4,10 +4,15 @@ import tkMessageBox
 top = Tkinter.Tk()
 
 def helloCallBack():
-   tkMessageBox.showinfo( "Hello Python", "Hello World")
+   tkMessageBox.showinfo( "gui-app", "Hello, World!")
+def badaCallback():
+   B3.pack()
+def showMoreCallback():
+   tkMessageBox.showerror("Sorry", "Not coded yet")
 
-B = Tkinter.Button(top, text ="Hello", command = helloCallBack)
-B2 = Tkinter.Button(top, text ="Badaboom!", command= helloCallBack)
+B = Tkinter.Button(top, text ="Hello", command = helloCallback)
+B2 = Tkinter.Button(top, text ="Badaboom!", command= badaCallback)
+B3 = Tkinter.Button(top, text ="Show More...", command= showMoreCallback)
 
 
 tkMessageBox.showerror("Willkommen!", "Welcome to gui-app")
