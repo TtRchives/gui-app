6df661c3a172568db3ff60f19b84fdb3393a3d87
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-09-10 07:42:05 -0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -26,7 +26,7 @@ def aboutCallback():
    about.title("About gui-app")
    #display message
    Label(about, text="gui-app 2.1 build 6").pack()
-   Label(about).pack
+   Label(about).pack()
    Label(about, text="CREDITS: (NOT AFFILIATED WITH ANY OF THESE SITES)").pack()
    Label(about).pack()
    Label(about, text="Thanks to tiny.cc/tkinterguide for providing a guide.").pack
@@ -35,7 +35,8 @@ def aboutCallback():
    Label(about, text="Thanks also to pythonprogramming.net/tkinter-adding-text-images/ for a nice article (can't remember what it taught me though)").pack()
    Label(about, text="Thanks so much to the Python Software Foundation and TkInter -- without you this wouldn't exist!").pack()
    Label(about, text="Thanks to smallguysit.com/index.php/2017/03/10/tkinter-create-window/ for 'from Tkinter import *'").pack()
-   Label(about).pack
+   Label(about, text="Thanks to tiny.cc/childwindow for the guide on child windows -- it helped with this window!").pack()
+   Label(about).pack()
    Label(about, text="Thanks to all!").pack()
    #quit child window and return to root window
    #the button is optional here, simply use the corner x of the child window
@@ -49,6 +50,7 @@ def showMoreCallback():
 def badaCB():
    BAbt.pack()
    Button(top, text='Exit', command=top.destroy).pack()
+   Button(top, command=easterEggCallback).pack()
 def messageWindow():
     #create child window
     newWindow = Toplevel()
@@ -58,20 +60,21 @@ def messageWindow():
     #quit child window and return to root window
     #the button is optional here, simply use the corner x of the child window
     Button(newWindow, text='OK', command=newWindow.destroy).pack()
+def easterEggCallback():
+  txt3.pack()
 
 B = t.Button(top, text ="Hello", command = helloCallback)
 BAbt = t.Button(top, text ="About...", command=aboutCallback)
 BSm = t.Button(top, text ="Show more...", command= badaCallback)
 BSem = t.Button(top, text ="Show even more...", command= showMoreCallback)
 BBada = t.Button(top, text ="Badaboom!", command=badaCB)
-BTest = t.Button(top, text= "Test the thing", command= messageWindow)
 txt1 = Label(top, text="'Hello' button was pressed")
 txt2 = Label(top, text="Welcome to gui-app!")
+txt3 = Label(top, text="NOTE: You pressed the Easter egg!")
 
 txt2.pack()
 B.pack()
 BSm.pack()
-BTest.pack()
 tkMB.showerror("Willkommen!", "Welcome to gui-app")
 
 
